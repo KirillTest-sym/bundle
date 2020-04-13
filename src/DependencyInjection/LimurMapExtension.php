56537f719d8e0d21e808166b57afe93a6c4d6fb5
Kirill Amurskii <kirillamurskii@gmail.com>
2020-04-13 16:35:18 +0300

    f

--- src/DependencyInjection/LimurMapExtension.php
+++ src/DependencyInjection/LimurMapExtension.php
@@ -3,6 +3,7 @@
 
 namespace Limur\Map\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
